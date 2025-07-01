9e8d70f92c061ea39c8abb479f155e6dd0c00059
muazkadan <muaz.kadan@gmail.com>
2025-07-01 22:40:54 +0300

    Add initial project files for ios target

---
+++ app/src/iosMain/kotlin/com/qamar/jetpackcomposscanner/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.qamar.jetpackcomposscanner
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { RecompositionTrackerScreen() }
